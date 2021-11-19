99f48865c1a69087ff59736a11ffd71b39ad0b1c
brymopo <bmoreno.dl@gmail.com>
2021-11-19 14:57:56 -0500

    add new and create actions to CsvFile controller, along views

--- app/controllers/csv_files_controller.rb
+++ app/controllers/csv_files_controller.rb
@@ -5,4 +5,47 @@ class CsvFilesController < ApplicationController
     @csv_files = current_user.csv_files
                              .paginate(page: params[:page], per_page: 20)
   end
+
+  def new
+    @csv_file = CsvFile.new
+  end
+
+  def create
+    csv_file = CsvFile.create(create_params)
+    if csv_file.valid?
+      ParserJob.perform_later({ file_id: csv_file.id, headers: file_headers })
+      flash[:notice] = "File imported correctly. Processing will start soon"
+      redirect_to csv_files_path
+    else
+      flash.now[:alert] = "Something went wrong, please see below"
+      render :new
+    end
+  end
+
+  private
+
+  def file_headers
+    result = {}
+    key_headers = %i[name address phone date_of_birth credit_card email]
+    key_headers.each do |key|
+      result[key] = csv_files_params[key] unless csv_files_params[key].blank?
+    end
+    result
+  end
+
+  def csv_files_params
+    params.require(:csv_file).permit(
+                                :file,
+                                :name,
+                                :address,
+                                :phone,
+                                :date_of_birth,
+                                :credit_card,
+                                :email
+                                 )
+  end
+
+  def create_params
+    { file: csv_files_params[:file], user: current_user }
+  end
 end

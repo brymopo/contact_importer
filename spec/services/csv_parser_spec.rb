2fe2210c1de0c78c35404228f7183839aae1ee08
brymopo <bmoreno.dl@gmail.com>
2021-11-19 07:57:41 -0500

    add CsvParser service specs

--- spec/services/csv_parser_spec.rb
+++ spec/services/csv_parser_spec.rb
@@ -15,13 +15,7 @@ RSpec.describe CsvParser, type: :model do
       record.save
       record
     end
-    let(:params) do
-      {
-        user_id: user.id,
-        file_id: csv_file.id,
-        headers: headers
-      }
-    end
+    let(:params) { { file_id: csv_file.id, headers: headers } }
 
     it "when headers are default and data correct" do
       expect(csv_file).to be_valid
@@ -85,6 +79,65 @@ RSpec.describe CsvParser, type: :model do
       it "scenario" do
         expect(csv_file).to be_valid
         expect { described_class.call(params) }.to change(Contact, :count).by(6)
+        expect(csv_file.reload).to be_finished
+      end
+    end
+
+    context "when headers are different and a column is missing" do
+      let(:file_name) { "different_headers.csv" }
+      let(:headers) do
+        {
+          name: "nombre",
+          phone: "telefono",
+          email: "correo",
+          date_of_birth: "fecha de nacimiento",
+          credit_card: "tarjeta de credito"
+        }
+      end
+
+      it "scenario" do
+        expect(csv_file).to be_valid
+        expect { described_class.call(params) }.not_to change(Contact, :count)
+        expect(csv_file.reload).to be_failed
+      end
+    end
+
+    context "when headers are different and empty rows" do
+      let(:file_name) { "different_headers_one_empty_row.csv" }
+      let(:headers) do
+        {
+          name: "nombre",
+          phone: "telefono",
+          address: "direccion",
+          email: "correo",
+          date_of_birth: "fecha de nacimiento",
+          credit_card: "tarjeta de credito"
+        }
+      end
+
+      it "scenario" do
+        expect(csv_file).to be_valid
+        expect { described_class.call(params) }.to change(Contact, :count).by(5)
+        expect(csv_file.reload).to be_finished
+      end
+    end
+
+    context "when csv_file id does not exist" do
+      let(:params) { { file_id: "doesnotexist", headers: headers } }
+
+      it "scenario" do
+        count_before = Contact.count
+        expect { described_class.call(params) }.to raise_error(ActiveRecord::RecordNotFound)
+        expect(Contact.count).to eq(count_before)
+      end
+    end
+
+    context "when csv_file has right headers and empty rows" do
+      let(:file_name) { "right_headers_one_empty_row.csv" }
+
+      it "scenario" do
+        expect(csv_file).to be_valid
+        expect { described_class.call(params) }.to change(Contact, :count).by(5)
         expect(csv_file.reload).to be_finished
       end
     end

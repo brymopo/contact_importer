c52f70eb2bef93e0044b0451e710b489f9620589
brymopo <bmoreno.dl@gmail.com>
2021-11-19 11:05:43 -0500

    add contacts and csv_files routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#index"
   devise_for :users
+  resources :csv_files
+  resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

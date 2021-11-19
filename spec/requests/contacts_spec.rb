3e8647542585f45c646a39ec1e17a901f074015d
brymopo <bmoreno.dl@gmail.com>
2021-11-19 11:05:10 -0500

    add tests for contacts and csv_files controllers

---
+++ spec/requests/contacts_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe ContactsController, type: :request do
+  let(:user) { create(:user) }
+
+  describe "GET /index" do
+    subject { get contacts_path }
+
+    context "when user is not logged in" do
+      it { is_expected.to redirect_to(new_user_session_path) }
+    end
+
+    context "when user is logged in and has no contacts" do
+      before do
+        sign_in user
+        subject
+      end
+
+      it { is_expected.to render_template(:index) }
+      it { expect(assigns(:contacts)).not_to be_nil }
+      it { expect(assigns(:contacts)).to be_blank }
+    end
+
+    context "when user is logged in and has files" do
+      let!(:test_contacts) { create_list(:contact, 25, user: user) }
+
+      before do
+        sign_in user
+        subject
+      end
+
+      it { is_expected.to render_template(:index) }
+      it { expect(assigns(:contacts)).to match_array(test_contacts.first(20)) }
+      it { expect(assigns(:contacts)).not_to include(test_contacts.last(5)) }
+    end
+  end
+end
---
+++ spec/requests/csv_files_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe CsvFilesController, type: :request do
+  let(:user) { create(:user) }
+
+  describe "GET /index" do
+    subject { get csv_files_path }
+
+    context "when user is not logged in" do
+      it { is_expected.to redirect_to(new_user_session_path) }
+    end
+
+    context "when user is logged in and has no files" do
+      before do
+        sign_in user
+        subject
+      end
+
+      it { is_expected.to render_template(:index) }
+      it { expect(assigns(:csv_files)).not_to be_nil }
+      it { expect(assigns(:csv_files)).to be_blank }
+    end
+
+    context "when user is logged in and has files" do
+      let!(:files) { create_list(:csv_file, 25, user: user) }
+
+      before do
+        sign_in user
+        subject
+      end
+
+      it { is_expected.to render_template(:index) }
+      it { expect(assigns(:csv_files)).to match_array(files.first(20)) }
+      it { expect(assigns(:csv_files)).not_to include(files.last(5)) }
+    end
+  end
+end
